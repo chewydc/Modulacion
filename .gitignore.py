7bc3ba224b3f412530e5dae18ac6b4ac118e168e
Damian Del Campo <ddelcampo@teco.com.ar>
2025-08-25 14:05:38 -0300

    add .gitgnore

---
+++ .gitignore.py
@@ -0,0 +1,19 @@
+# Archivos de Python
+*.pyc
+*.pyo
+*.pyd
+__pycache__/
+*.so
+
+# Entornos virtuales
+.venv/
+env/
+venv/
+
+# Jupyter Notebook checkpoints
+.ipynb_checkpoints
+
+# Archivos de configuración de IDE
+.vscode/
+.idea/
+*.iml
